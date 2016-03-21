ca1191188402f44690f5ff4848ebfaed2d10c368
Radivarig <reslav.hollos@gmail.com>
2016-03-21 21:57:23 +0100

    (move) demo code to viewer component

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,36 +1,8 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var InfiniteAnyHeight = require('./InfiniteAnyHeight.jsx')
-
-var App = React.createClass({
-  getInitialState() {
-    var list =
-      new Array(10000).fill('').map((x, i)=>{
-        var height = 100 +Math.round(Math.abs((Math.sin(i) *250)))
-        return (
-          <div key={i} style={{height: height, width: '100wv', border: 'solid 1px', backgroundColor: '#' +height}}>
-            height: {height +2 /*border width*/}
-            <div style={{textAlign: 'center', color: 'white'}}>
-              item: {i}
-            </div>
-          </div>
-        )
-      })
-    return {list}
-  },
-
-  render: function() {
-    return (
-      <InfiniteAnyHeight
-        list={this.state.list}
-        preloadAdditionalHeight={window.innerHeight*2}
-        useWindowAsScrollContainer
-        />
-    )
-  }
-})
+var InfiniteAnyHeightViewer = require('./InfiniteAnyHeightViewer.jsx')
 
 var elemDiv = document.createElement('div')
 document.body.appendChild(elemDiv)
-ReactDOM.render(<App/>, elemDiv)
+ReactDOM.render(<InfiniteAnyHeightViewer />, elemDiv)
